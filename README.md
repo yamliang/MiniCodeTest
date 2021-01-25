14a0db7a98c8cb1975c2952391be997be50aebfd
mingyanl <51693387+mingyanl@users.noreply.github.com>
2021-01-25 14:55:32 +0800

    add lettercombination util
    LetterCombinationUtil and LetterCombinationTest

---
+++ README.md
@@ -0,0 +1,22 @@
+# MiniCode Test Project
+
+<br/>
+
+　　This project is used to provide some common util programs, or do some code tests.
+
+### Bug list：
+
+#### package org.yan.util:all util programs
+    src/org/yan/LetterCombinationUtil.java
+
+<br/>
+
+    A util program to convert the digits from 0 to 9 into letters
+
+<br/>
+
+    srcTest/org/yan/LetterCombinationTest.java
+
+<br/>
+
+    The Junit test case for LetterCombinationUtil
---
+++ srcTest/org/yan/util/LetterCombinationTest.java
@@ -0,0 +1,33 @@
+package org.yan.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class LetterCombinationTest {
+
+	/**
+	 * Test {@link LetterCombinationUtil#letterCombination(Integer[])}}
+	 */
+	@Test
+	public void letterCombinationTest() {
+		Integer input0[] = new Integer[] {0, 3, 1, 4};
+		Integer input1[] = new Integer[] {0, 2, 5, 4, 7};
+		Integer input2[] = new Integer[] {2, 3, 4, 0, 10, 11, 00, 20, 99};
+		
+		List<String> output0 = LetterCombinationUtil.letterCombination(input0);
+		List<String> output1 = LetterCombinationUtil.letterCombination(input1);
+		List<String> output2 = LetterCombinationUtil.letterCombination(input2);
+		
+		assertEquals(output0.size(), 3 * 3);
+		assertEquals(output1.size(), 3 * 3 * 3 * 4);
+		assertEquals(output2.size(), 3 * 3 * 3 * 3 * 4 * 4);
+		
+		System.out.println(output0);
+		System.out.println(output1);
+		System.out.println(output2);
+	}
+
+}
